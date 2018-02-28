9cd676c0296141b1da022e0f43af271fe852a4f5
Laserbird1 <36840241+Laserbird1@users.noreply.github.com>
2018-02-28 17:20:40 +0100

    Update Monstre.java

--- Monstre.java
+++ Monstre.java
@@ -2,6 +2,20 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Monstre{
-    int vie;
-    int vitesse;
+    int vie;        //points de vie
+    int vitesse;    //vitesse de déplacement
+    int degats;     //degats infligés lors de la rencontre avec le chateau
+    int centrex;    //coord x du centre de gravité 
+    int centrey;    //coord y du centre de gravité
+    int L;          //largeur de la hitbox
+    int H;          //hauteur de la hitbox
+    
+    Monstre(){
+        L=50;//en pixel
+        H=70;//en pixel
+    }
+    
+    public void move(){
+        this.centrex+=vitesse;///ou -= selon orientation du chateau
+    }
 }
