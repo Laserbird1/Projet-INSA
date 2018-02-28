53bc10d066f25a7431a7e88f6c3b3e2553bf0189
Laserbird1 <36840241+Laserbird1@users.noreply.github.com>
2018-02-28 17:25:44 +0100

    Add files via upload

---
+++ Gobelin.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.imageio.*;
+import java.awt.geom.*;
+import java.awt.image.*;
+import java.io.*;
+
+public class Gobelin extends Monstre{
+    
+    Gobelin(){
+        super();
+        this.vie=2;
+        this.vitesse=50;
+        this.degats=1;
+        this.centrex=600;///a initialiser proprement
+        this.centrey=590;///a initialiser proprement
+        
+    }
+    
+    
+}
