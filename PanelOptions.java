cdbfcd8a31bcc0f60b8cb4053efaafe30dde833b
Dikec <36924737+Dikec@users.noreply.github.com>
2018-03-05 09:57:47 +0100

    Update PanelOptions.java

--- PanelOptions.java
+++ PanelOptions.java
@@ -13,7 +13,8 @@ public class PanelOptions extends JPanel implements ActionListener {
 		
 		JLabel mesOptions = new JLabel("Options");
 		mesOptions.setBounds((int)((2/5)*L),(int)((3/7)*H), (int)(L/5), (int)(H/7));
-		
+		Font parametre = new Font("Arial",Font.BOLD,75);
+		mesOptions.setFont(parametre);
 		
 		add(mesOptions);
 		setVisible(true);
