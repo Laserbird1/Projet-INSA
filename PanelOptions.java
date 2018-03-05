ea6d63e9083f2f244d98f4e93a21c0095f5d5647
Dikec <36924737+Dikec@users.noreply.github.com>
2018-03-05 09:48:15 +0100

    Add files via upload

---
+++ PanelOptions.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+
+public class PanelOptions extends JPanel implements ActionListener {
+	
+	public PanelOptions (int L,int H) {
+		
+		this.setLayout(null);
+		this.setSize(L,H);
+		
+		JLabel mesOptions = new JLabel("Options");
+		mesOptions.setBounds((int)((2/5)*L),(int)((3/7)*H), (int)(L/5), (int)(H/7));
+		
+		
+		add(mesOptions);
+		setVisible(true);
+		
+	}
+	public void actionPerformed ( ActionEvent e){
+	
+	}
+}
+
