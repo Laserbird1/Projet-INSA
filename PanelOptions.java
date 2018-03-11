7dd3e47e3a80faf6200b8fa90fc23da81b8d5fd0
Laserbird1 <36840241+Laserbird1@users.noreply.github.com>
2018-03-11 13:38:44 +0100

    Update PanelOptions.java

--- PanelOptions.java
+++ PanelOptions.java
@@ -5,9 +5,13 @@ import java.awt.event.*;
 
 
 public class PanelOptions extends JPanel implements ActionListener {
+	FenetreJeu Frame;
+    	JButton bMenu;
 	
 	public PanelOptions (int L,int H) {
 		
+	 	this.Frame=Frame;
+        
 		this.setLayout(null);
 		this.setSize(L,H);
 		
@@ -15,13 +19,21 @@ public class PanelOptions extends JPanel implements ActionListener {
 		mesOptions.setBounds((int)((2/5)*L),(int)((3/7)*H), (int)(L/5), (int)(H/7));
 		Font parametre = new Font("Arial",Font.BOLD,75);
 		mesOptions.setFont(parametre);
-		
 		add(mesOptions);
+		
+		bMenu=new JButton("Menu");
+		bMenu.setBounds((int)(L*3/5),(int)(H*1/10),L_BOUTON,H_BOUTON);
+		bMenu.addActionListener(this);
+		this.add(bMenu);
+		
 		setVisible(true);
 		
 	}
 	public void actionPerformed ( ActionEvent e){
-	
+		
+		if(e.getSource()==bMenu){
+		    Frame.setContentPane(Frame.panelMenu);
+		}
 	}
 }
 
