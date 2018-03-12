bca6d6aa93c92749639916ab7bfeea593cfb023d
Laserbird1 <36840241+Laserbird1@users.noreply.github.com>
2018-03-12 08:30:44 +0100

    Update Monstre.java

--- Monstre.java
+++ Monstre.java
@@ -6,26 +6,28 @@ import java.io.*;
 
 public abstract class Monstre{
     int vie;        //points de vie
-    int vitesse;    //vitesse de déplacement
+    int VIE_MAX;
+    double vitesse;    //vitesse de déplacement
     int degats;     //degats infligés lors de la rencontre avec le chateau
     
-    int centrex;    //coord du centre de gravité, référentiel a partir duquel est 
-    int centrey;    //créé l'image du monstre, permet le déplacement facile du monstre
+    double centrex;    //coord du centre de gravité, référentiel a partir duquel est 
+    double centrey;    //créé l'image du monstre, permet le déplacement facile du monstre
                     //Est en réalité le coin supérieur droit du monstre
     
-    int L;          //largeur de la hitbox
-    int H;          //hauteur de la hitbox
+    int L;          //dimensions hitbox
+    int H;          //Largeur,Hauteur
     
-    BufferedImage image ; //image associee au monstre
-    Color couleur;
+    Color couleur;///tant que pas de photos
     
-    Monstre(){
-        L=50;//en pixel
-        H=70;//en pixel
+   /// BufferedImage image ; //image associee au monstre
+    
+    Monstre(int L,int H){
+        this.L=L;//en pixel
+        this.H=H;//en pixel
     }
     
     public void move(){
-        this.centrex+=vitesse;///ou -= selon orientation du chateau
+        this.centrex-=vitesse;
     }
     
     public boolean collisionChateau(Chateau castle){
@@ -38,7 +40,22 @@ public abstract class Monstre{
     }
     
     public void dessin(Graphics g){
+        
         g.setColor(this.couleur);
-        g.fillRect(centrex, centrey,L,H);
+        g.fillRect((int)(centrex), (int)(centrey),L,H);
+        
+        //barre de vie
+        int lBarre=L; //longeur barre (longueur du monstre)
+        int l1=(int)(centrex);              //debut barre (coord x)
+        int y1=(int)(centrey-30);           //(coord y)
+        int l2=(int)(l1+(lBarre*(vie)/VIE_MAX)); //coord x séparation vert/noir
+        int l3=lBarre+l1;//coord x en pixel de la fin de la barre
+        int h=(int)(H/6);//hauteur barre
+        
+        g.setColor(new Color(46, 139, 87));//vert océan (vie restante)
+        g.fillRect(l1,y1,l2-l1,h);
+        
+        g.setColor(Color.black);//noir (vie perdue)
+        g.fillRect(l2,y1,l3-l2,h);
     }
 }
