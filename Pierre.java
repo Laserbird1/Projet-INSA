1e8e81dab9aecd0625295ebeccc337e174a072ce
Laserbird1 <36840241+Laserbird1@users.noreply.github.com>
2018-03-12 08:33:27 +0100

    Update Pierre.java

--- Pierre.java
+++ Pierre.java
@@ -8,30 +8,30 @@ import java.io.*;
 public class Pierre extends Projectile{
     int R;      //rayon de la pierre, pour la hitbox
 
-    Pierre(double tetaIni,double vitesseIni){ 
+    Pierre(double tetaIni,double vitesseIni,int R,int centrex,int centrey){ 
         super();
         this.vitessex=(int)(vitesseIni*Math.cos(tetaIni));
         this.vitessey=(int)(vitesseIni*Math.sin(tetaIni));  //assignation des valeurs de vitesses initiales
         
-        this.centrex=100; //positionnement initial
-        this.centrey=150;///assigner valeurs de la pos initiale, haut du chateau
+        this.centrex=centrex; //positionnement initial
+        this.centrey=centrey;
         
-        this.reloadTime=900;
+        this.reloadTime=800;
         this.degats=3;
         
-        this.R=50;
+        this.R=R;
         ///image
     }
     
     public void dessin(Graphics g){
-        g.setColor(new Color(192, 192, 192));//gris argent
-        g.drawOval(centrex,centrey,2*R,2*R);
+        g.setColor(new Color(255,222,173));//Jaune blanc navaro
+        g.fillOval(centrex,centrey,2*R,2*R);
     }
     
     public boolean collisionMonstre(Monstre monster){
         
         Ellipse2D.Double box1 = new Ellipse2D.Double(centrex,centrey,2*R,2*R);               //On crée une hitbox pour le projectile
-        Rectangle box2 = new Rectangle(monster.centrex,monster.centrey,monster.L,monster.H); //on crée une hitbox pour le monstre
+        Rectangle box2 = new Rectangle((int)(monster.centrex),(int)(monster.centrey),monster.L,monster.H); //on crée une hitbox pour le monstre
         return box1.intersects(box2);                                                        //sont-ils en collision ?
         
     }
