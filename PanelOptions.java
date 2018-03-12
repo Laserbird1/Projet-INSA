56fd8d07d124749d972ef1d9bd8e11483a642513
Laserbird1 <36840241+Laserbird1@users.noreply.github.com>
2018-03-12 08:31:35 +0100

    Update PanelOptions.java

--- PanelOptions.java
+++ PanelOptions.java
@@ -5,35 +5,40 @@ import java.awt.event.*;
 
 
 public class PanelOptions extends JPanel implements ActionListener {
-	FenetreJeu Frame;
-    	JButton bMenu;
+    
+    FenetreJeu Frame;
+    JButton bMenu;
 	
-	public PanelOptions (int L,int H, FenetreJeu Frame) {
+	public PanelOptions (int L,int H,FenetreJeu Frame) {
 		
-	 	this.Frame=Frame;
+        this.Frame=Frame;
         
 		this.setLayout(null);
 		this.setSize(L,H);
+        
+        int L_BOUTON=(int)(L/12);
+        int H_BOUTON=(int)(H/12);
 		
 		JLabel mesOptions = new JLabel("Options");
 		mesOptions.setBounds((int)((2/5)*L),(int)((3/7)*H), (int)(L/5), (int)(H/7));
 		Font parametre = new Font("Arial",Font.BOLD,75);
 		mesOptions.setFont(parametre);
 		add(mesOptions);
-		
-		bMenu=new JButton("Menu");
-		bMenu.setBounds((int)(L*3/5),(int)(H*1/10),L_BOUTON,H_BOUTON);
-		bMenu.addActionListener(this);
-		this.add(bMenu);
-		
+        
+        bMenu=new JButton("Menu");
+        bMenu.setBounds((int)(L*3/5),(int)(H*1/10),L_BOUTON,H_BOUTON);
+        bMenu.addActionListener(this);
+        this.add(bMenu);
+        
 		setVisible(true);
+        
 		
 	}
 	public void actionPerformed ( ActionEvent e){
-		
-		if(e.getSource()==bMenu){
-		    Frame.setContentPane(Frame.panelMenu);
-		}
+        if(e.getSource()==bMenu){
+            Frame.setContentPane(Frame.panelMenu);
+        }
 	}
 }
+
 
